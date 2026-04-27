f70dfc380e7b4c062fdcadeb295301bdf5177688
Joshua A. Sosmeña <20-1-01709@vsu.edu.ph>
2026-04-27 21:50:55 +0800

    attempt #12 for fixing firebase upload error

--- lib/firebase/firebase-admin.ts
+++ lib/firebase/firebase-admin.ts
@@ -1,25 +1,21 @@
 import * as admin from "firebase-admin";
 
 if (!admin.apps.length) {
-  // SAFETY CHECK: This will throw a clear error in Vercel if the variable is missing
-  if (!process.env.FIREBASE_PRIVATE_KEY_BASE64) {
-    console.error(
-      "CRITICAL ERROR: FIREBASE_PRIVATE_KEY_BASE64 is missing in this environment!",
-    );
-  }
-
-  const privateKey = Buffer.from(
-    process.env.FIREBASE_PRIVATE_KEY_BASE64 || "",
-    "base64",
-  )
-    .toString("utf8")
-    .trim();
-
+  console.log("[firebase-admin] Initializing Firebase Admin SDK");
+  console.log(
+    "[firebase-admin] Check Private Key if Exists:",
+    !!process.env.FIREBASE_PRIVATE_KEY,
+  );
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: privateKey,
+      // 1. replace(/\\n/g, "\n") handles escaped strings (if any)
+      // 2. replace(/\r/g, "") destroys the hidden carriage returns causing the OpenSSL crash
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(
+        /\\n/g,
+        "\n",
+      ).replace(/\r/g, ""),
     }),
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   });
