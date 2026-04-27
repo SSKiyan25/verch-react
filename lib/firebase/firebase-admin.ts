ec195a98fd49226188cb4e3cb0ced529681883d4
Joshua A. Sosmeña <20-1-01709@vsu.edu.ph>
2026-04-27 20:55:22 +0800

    attempt #9 for fixing firebase upload error

--- lib/firebase/firebase-admin.ts
+++ lib/firebase/firebase-admin.ts
@@ -1,16 +1,17 @@
 import * as admin from "firebase-admin";
 
 if (!admin.apps.length) {
+  // Decode the Base64 string safely back into a multiline PEM format
+  const privateKey = Buffer.from(
+    process.env.FIREBASE_PRIVATE_KEY_BASE64 || "",
+    "base64",
+  ).toString("ascii");
+
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      // 1. replace(/\\n/g, "\n") handles escaped strings (if any)
-      // 2. replace(/\r/g, "") destroys the hidden carriage returns causing the OpenSSL crash
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(
-        /\\n/g,
-        "\n",
-      ).replace(/\r/g, ""),
+      privateKey: privateKey,
     }),
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   });
